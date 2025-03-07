573c5aac8dae9c76052f407478d381a9ae53a20f
bruhwattsondocha <161598560+bruhwattsondocha@users.noreply.github.com>
2025-03-07 16:02:34 +0300

    Add createGameboard fn
    Creates 3x3 array for storing player markers

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 'use strict';
 
-
+const createGameboard = (() => {
+  const gameboard = [];
+  for (let i = 0; i < 3; i++) {
+    gameboard.push([])
+    for (let j = 0; j < 3; j++) {
+      gameboard[i].push('');
+    }
+  }
+  return gameboard;
+})();
